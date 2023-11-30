82b4b9c438aefcf696b8e992e7b8c0f2b0740c57
seoyeon <seoyeonn051@gmail.com>
2023-12-01 03:36:08 +0900

    fix: applicationContext 추가

--- app/src/main/java/com/example/mvvm_practice/application/AndroidApp.kt
+++ app/src/main/java/com/example/mvvm_practice/application/AndroidApp.kt
@@ -1,11 +1,17 @@
 package com.example.mvvm_practice.application
 
 import android.app.Application
+import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class AndroidApp:Application() {
+    companion object{
+        lateinit var context: Context
+
+    }
     override fun onCreate() {
         super.onCreate()
+        context = applicationContext
     }
 }
